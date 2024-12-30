9d38d2e8da2cb96de0214a8ae62d075edcd65b48
Молдабаев Алиби <alibi.moldabaev@ylab.team>
2024-12-30 15:06:04 +0500

    added WaitingRoutesService which manages the route queue via

---
+++ office/src/main/java/com/alibi/control_room_jet/office/service/PathService.java
@@ -0,0 +1,9 @@
+package com.alibi.control_room_jet.office.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PathService {
+}
